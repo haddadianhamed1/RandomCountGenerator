a62004a8689b5ecde03eb2d5d8ef93daca57c37b
haddadianhamed1 <hamedh@dnanexus.com>
2018-01-14 13:43:41 -0800

    added class for generating and calculating the frequency of the numbers generated

--- random-count-generator.py
+++ random-count-generator.py
@@ -1,5 +1,7 @@
 import numpy
 from collections import defaultdict
+import abc
+import Queue
 
 """
 Create a function that prints a random number between 1 and 5 to stdout (or console). The probability distribution of the numbers should be as follows:
@@ -21,8 +23,71 @@ for i in (range(100)):
 print random_number
 
 """
+
+# using numpy printing random number with specific weight
 def random_number():
     rn=numpy.random.choice(numpy.arange(1, 6), p=[0.5, 0.25, 0.15, 0.05, 0.05])
     print rn
 
-random_number()
+# random_number()
+
+
+# abstract class for tracking
+class GetterSetter(object):
+    __masterclass__ = abc.ABCMeta
+    @abc.abstractmethod
+    def generator(self):
+        """generates random number"""
+        return
+
+    @abc.abstractmethod
+    def get_history(self):
+        """list of last n random numbers"""
+
+        return
+
+    @abc.abstractmethod
+    def append_history(self):
+        """adds number to the list of random numbers and removes oldest entry if list goes above 100 numbers"""
+
+        return
+
+    @abc.abstractmethod
+    def cal_stat(self):
+        """calculates the frequency of generated numbers"""
+
+        return
+# class to generate and queue the history
+# abstract class from GetterSetter
+class Generator(GetterSetter):
+    history = Queue.Queue(maxsize=100)
+    def generator(self):
+        rn = numpy.random.choice(numpy.arange(1, 6), p=[0.5, 0.25, 0.15, 0.05, 0.05])
+        self.append_history(rn)
+        # print random number
+        #print rn
+    def get_history(self):
+        return list(self.history.queue)
+    def append_history(self,number):
+        if self.history.qsize() >= 100:
+            self.history.get()
+        self.history.put(number)
+    def cal_stat(self):
+        random_number = defaultdict(int)
+        for numbers in list(self.history.queue):
+            random_number[numbers]+=1
+
+        for key,value in random_number.iteritems():
+            print key, str(value/float(self.history.qsize())*100) + '%'
+
+
+
+# testing
+x = Generator()
+for y in range (500):
+    x.generator()
+
+print x.get_history()
+print len(x.get_history())
+
+x.cal_stat()
